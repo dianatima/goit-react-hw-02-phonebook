3f7cb30fd0e85a5f11a5e2dc1272fab19c7f259e
Yulia <nayanzina@gmail.com>
2023-02-14 12:54:18 +0200

    phonebook

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import { FilterWrap } from './Filter.styled';
+
+export const Filter = ({ value, onChange}) => {
+    return (
+        <FilterWrap>
+            <h3>Find contact by name: </h3>
+            <input
+                type="text"
+                name="filter"
+                onChange={onChange}
+                value={value}
+            />
+        </FilterWrap>
+    )
+} 
